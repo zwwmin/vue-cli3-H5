8cb56ffe790e0050bf0a4da7fbb6fbaeb830ce0c
zww <zwwemi@gmail.com>
2019-06-20 14:44:28 +0800

    one

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,7 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <h1>1</h1>
     </ul>
   </div>
 </template>
